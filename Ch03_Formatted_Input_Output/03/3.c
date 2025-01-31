51384035077f477566f8116fb944143f4c840ae1
MorningStar1314 <104242332+MorningStar1314@users.noreply.github.com>
2025-01-31 21:21:23 +0800

    Fix Bug

--- Ch03_Formatted_Input_Output/03/3.c
+++ Ch03_Formatted_Input_Output/03/3.c
@@ -7,10 +7,10 @@ int main() {
     printf("Enter ISBN: ");
     scanf("%d-%d-%d-%d-%d", &prefix, &identifier, &code, &number, &digit);
 
-    printf("GS1 prefix: %3d\n", prefix);
+    printf("GS1 prefix: %.3d\n", prefix);
     printf("Group identifier: %d\n", identifier);
-    printf("Publisher code: %3d\n", code);
-    printf("Item number: %5d\n", number);
+    printf("Publisher code: %.3d\n", code);
+    printf("Item number: %.5d\n", number);
     printf("Check digit: %d\n", digit);
 
     return 0;
--- Ch03_Formatted_Input_Output/04/4.c
+++ Ch03_Formatted_Input_Output/04/4.c
@@ -7,7 +7,7 @@ int main() {
     printf("Enter phone number [(xxx) xxx-xxxx]: ");
     scanf("(%d) %d-%d", &prefix, &three, &four);
 
-    printf("You entered %03d.%03d.%04d", prefix, three, four);
+    printf("You entered %.3d.%.3d.%.4d", prefix, three, four);
 
     return 0;
 }
