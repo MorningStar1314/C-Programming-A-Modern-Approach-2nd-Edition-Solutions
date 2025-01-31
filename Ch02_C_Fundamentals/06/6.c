2e60b3f74e6c8014c83678661efe95172d6c0b1c
MorningStar1314 <104242332+MorningStar1314@users.noreply.github.com>
2025-01-31 20:30:27 +0800

    Add project 6

--- Ch02_C_Fundamentals/06/6.c
+++ Ch02_C_Fundamentals/06/6.c
@@ -6,7 +6,7 @@ int main() {
 
     printf("Enter a value for x: ");
     scanf("%d", &x);
-    printf("Result: %d", 3 * x * x * x * x * x + 2 * x * x * x * x - 5 * x * x * x - x * x + 7 * x - 6);
-
+    printf("Result: %d", ((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6);
+    
     return 0;
 }
